3043fa7ea910f7db75f883b4c71aca26d738a22d
Sabrina <binafbueno@gmail.com>
2022-05-04 18:05:07 -0300

    ex016

---
+++ aula14Ex/ex016/script.js
@@ -0,0 +1,26 @@
+function contar () {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Faltam dados!')  
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f) {
+            for (var c = i; c <= f; c+= p) {
+            res.innerHTML += `${c} \u{1f449}`
+            }   
+    } else {
+        for (var c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{1f449}`
+        }
+    }
+    res.innerHTML += `\u{1f3c1}`
+    }
+}
